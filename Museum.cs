66f393d27e7160fcc408ed49d8dbdee7c372d599
Edvinas Balčiūnas <edvinoske@inbox.lt>
2016-11-06 17:28:19 +0200

    Added missing class to git

---
+++ Museum.cs
@@ -0,0 +1,27 @@
+﻿namespace U4___Rework
+{
+    class Museum : PlaceOfInterest
+    {
+        public string Type { get; set; }
+        //Darbo dienos
+        public bool[] WorksOn { get; set; }
+        public double TicketPrice { get; set; }
+        public bool HasGuide { get; set; }
+
+        public Museum() { }
+
+        public Museum(string name, string adress, int year, string type, bool[] worksOn, double ticketPrice, bool hasGuide)
+            : base(name, adress, year)
+        {
+            Type = type;
+            WorksOn = worksOn;
+            TicketPrice = ticketPrice;
+            HasGuide = hasGuide;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0,-10}, {1,-10}, {2,-10}, {3}, {4,-10}, {5,-10}", Name, Adress, Year, string.Join(",", WorksOn), TicketPrice, HasGuide);
+        }
+    }
+}
